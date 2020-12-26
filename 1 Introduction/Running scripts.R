b6c6e6010e7abbbe72d6040fe2b2d8c282e61fa2
Martin Elff <elff@mobitux11f.fritz.box>
2020-12-26 20:57:56 +0100

    Some improvements for the introduction

---
+++ 1 Introduction/Running scripts.R
@@ -0,0 +1,4 @@
+## Running a script with and without echo
+source("source-echo-demo.R")
+
+source("source-echo-demo.R",echo=TRUE)
---
+++ 1 Introduction/Saving and loading objects.R
@@ -0,0 +1,12 @@
+# In this example we create the two variables 'a' and 'b':
+a <- 1
+b <- 1
+# We save them in the data file "ab.RData"
+save(a,b,file="ab.RData")
+# We then remove the two variables:
+rm(a,b)
+# With then load the data file
+load("ab.RData")
+# and verify that the two variables are restored
+a
+b
